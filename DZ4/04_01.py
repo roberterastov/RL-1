0068053b888f1d3aa18864dc2100cc50cd715e4e
Robert Erastov <robertino2006@yandex.ru>
2022-03-01 19:18:48 +0300

    Домашнее задание к 4 уроку

---
+++ DZ4/04_01.py
@@ -0,0 +1,25 @@
+'''
+# 1.Реализовать скрипт, в котором должна быть предусмотрена
+ функция расчета заработной платы сотрудника. В расчете
+ необходимо использовать формулу:
+ (выработка в часах*ставка в час) + премия.
+ Для выполнения расчета для конкретных значений необходимо
+ запускать скрипт с параметрами.
+'''
+# prize - ну куда ж без премии))
+from sys import argv
+
+if len(argv) > 1:
+    name_script, time_work, rate, prize = argv
+    time_work = int(time_work)
+    rate = int(rate)
+    prize = int(prize)
+    print((time_work * rate) + prize)
+else:
+    time_work = int(input("Inpute time work: "))
+    rate = int(input("Inpute ratet: "))
+    prize = int(input("Inpute prize: "))
+    print((time_work * rate) + prize)
+
+
+
